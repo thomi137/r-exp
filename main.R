4d50c11c1164aab56ed91ea83f7bbd7653ed23db
tprosser <thomas@prosser.ch>
2019-10-08 13:40:10 +0200

    some musings

--- main.R
+++ main.R
@@ -1,4 +1,12 @@
 library(ggplot2)
 
 summary(amazon)
+amazon$year <- factor(amazon$year)
+str(amazon)
+summary(amazon)
+
+fire.per.year <- aggregate(amazon$number, by=list(year=amazon$year, state=amazon$state), FUN=sum)
+
+ggplot(fire.per.year, aes(year, x)) + 
+  geom_boxplot()
 
